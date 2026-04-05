666cb58f3393d49313c2f42a7c2621da9efff713
gharneeyart <shuaibganiyat20@gmail.com>
2026-04-05 20:56:14 +0100

    fix: deployment issue

--- frontend/src/pages/admin/AdminApplicationsPage.tsx
+++ frontend/src/pages/admin/AdminApplicationsPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import {
